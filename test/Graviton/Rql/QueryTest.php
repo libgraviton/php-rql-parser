fdc48b1938652e0844d11d6a18d72d3ee1696dda
Lucas Bickel <lucas.bickel@swisscom.com>
2015-02-03 12:52:17 +0100

    tests for sort() with ascending and descending as well as multi searches

--- test/Graviton/Rql/QueryTest.php
+++ test/Graviton/Rql/QueryTest.php
@@ -117,4 +117,56 @@ class QueryTest extends \PHPUnit_Framework_TestCase
         $sut->applyToQueriable($mock);
     }
 
+    /**
+     * test for sort spec
+     */
+    public function testSortCalls()
+    {
+        $sut = new \Graviton\Rql\Query('sort(name)');
+
+        $mock = $this->getMock('\Graviton\Rql\QueryInterface');
+
+        $mock->expects($this->once())
+             ->method('sort')
+             ->with($this->equalTo('name'));
+
+        $sut->applyToQueriable($mock);
+    }
+
+    /**
+     * test for ascending sort
+     */
+    public function testAscendingSort()
+    {
+        $sut = new \Graviton\Rql\Query('sort(-name)');
+
+        $mock = $this->getMock('\Graviton\Rql\QueryInterface');
+
+        $mock->expects($this->once())
+             ->method('sort')
+             ->with($this->equalTo('name'), $this->equalTo('asc'));
+
+        $sut->applyToQueriable($mock);
+    }
+
+    /**
+     * test multi sort
+     */
+    public function testMultiSort()
+    {
+        $sut = new \Graviton\Rql\Query('sort(+price,-name)');
+
+        $mock = $this->getMock('\Graviton\Rql\QueryInterface');
+
+        $mock->expects($this->at(0))
+             ->method('sort')
+             ->with($this->equalTo('price'), $this->equalTo('desc'));
+        $mock->expects($this->at(1))
+             ->method('sort')
+             ->with($this->equalTo('name'), $this->equalTo('desc'));
+
+        $sut->applyToQueriable($mock);
+    }
+
+
 }
