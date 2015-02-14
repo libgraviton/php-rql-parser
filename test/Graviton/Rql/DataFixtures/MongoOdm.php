2d0550c34a82685271df217dcbcdf2e5ff6c2740
Lucas Bickel <lucas.bickel@swisscom.com>
2015-02-14 14:26:44 +0100

    test fixtures for mongodb

---
+++ test/Graviton/Rql/DataFixtures/MongoOdm.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * small collection of basic features
+ */
+
+namespace Graviton\Rql\DataFixtures;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Graviton\Rql\Queriable\Documents\Foo;
+
+/**
+ * Make up some fixtures for testing
+ *
+ * @category MongoODM
+ * @package  RqlParser
+ * @author   Lucas Bickel <lucas.bickel@swisscom.com>
+ * @license  http://opensource.org/licenses/MIT MIT License (c) 2015 Swisscom
+ * @link     http://swisscom.ch
+ */
+class MongoOdm implements FixtureInterface
+{
+    /**
+     * load fixtures with the passed em
+     *
+     *  @param ObjectManager $manager
+     */
+    public function load (ObjectManager $manager)
+    {
+        $doc = new Foo;
+        $doc->name = 'My First Sprocket';
+        $doc->count = 10;
+
+        $manager->persist($doc);
+
+        $widget = new Foo;
+        $widget->name = 'A Simple Widget';
+        $widget->count = 100;
+
+        $manager->persist($widget);
+
+        $manager->flush();
+    }
+}
