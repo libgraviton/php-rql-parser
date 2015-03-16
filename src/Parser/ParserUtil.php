0364f49a9a9474349693b21783a5a126dfe0a7fc
Lucas Bickel <lucas.bickel@swisscom.com>
2015-03-16 01:59:35 +0100

    simplyfy string parameter parsing code

--- src/Parser/ParserUtil.php
+++ src/Parser/ParserUtil.php
@@ -55,14 +55,7 @@ class ParserUtil
         $lexer->moveNext();
         $string = null;
         if ($lexer->lookahead['type'] == Lexer::T_STRING) {
-            $string = $lexer->lookahead['value'];
-            $glimpse = $lexer->glimpse();
-            if ($glimpse['type'] == Lexer::T_MINUS ||
-                $glimpse['type'] == Lexer::T_PLUS
-            ) {
-                $lexer->moveNext();
-                $string = $string . $lexer->lookahead['value'] . self::getString($lexer);
-            }
+            $string = self::getString($lexer, false);
         } elseif ($lexer->lookahead['type'] == Lexer::T_INTEGER) {
             $string = (int) $lexer->lookahead['value'];
         } else {
