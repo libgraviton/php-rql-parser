c3c6d2d6eebdc79229c4a17f1eac786c15032a4c
lapistano <git@bastian-feder.de>
2015-03-19 10:33:59 +0100

    Fixed whitespaces

--- src/AST/OperationInterface.php
+++ src/AST/OperationInterface.php
@@ -9,8 +9,8 @@ use Graviton\Rql\Visitor\VisitorInterface;
 
 /**
  * @author  List of contributors <https://github.com/libgraviton/php-rql-parser/graphs/contributors>
- * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
- * @link     http://swisscom.ch
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link    http://swisscom.ch
  */
 interface OperationInterface
 {
--- src/Lexer.php
+++ src/Lexer.php
@@ -7,8 +7,8 @@ namespace Graviton\Rql;
 
 /**
  * @author  List of contributors <https://github.com/libgraviton/php-rql-parser/graphs/contributors>
- * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
- * @link     http://swisscom.ch
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link    http://swisscom.ch
  */
 class Lexer extends \Doctrine\Common\Lexer
 {
