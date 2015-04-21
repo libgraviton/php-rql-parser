15873146f5577a748e970d52fe55c999ed7f9cd9
Lucas Bickel <lucas.bickel@swisscom.com>
2015-04-21 20:28:57 +0200

    Fix line length issues

--- src/Parser.php
+++ src/Parser.php
@@ -104,7 +104,11 @@ class Parser
         }
         if (!$operation) {
             throw new \RuntimeException(
-                sprintf("No strategies matched the type %s (%s). Did you load all strategies?", $type, $this->lexer->lookahead['value'])
+                sprintf(
+                    'No strategies matched the type %s (%s). Did you load all strategies?',
+                    $type,
+                    $this->lexer->lookahead['value']
+                )
             );
         }
         return $operation;
