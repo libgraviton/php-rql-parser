93601428f09898e18aa1737a4f5702c1ab5586da
Dario Nuevo <wee@xbe.ch>
2023-03-13 14:00:38 +0100

    return correct node

--- src/NodeParser/ProjectNodeParser.php
+++ src/NodeParser/ProjectNodeParser.php
@@ -1,12 +1,18 @@
 <?php
 namespace Graviton\Rql\NodeParser;
 
+use Graviton\Rql\Node\ProjectNode;
+use Graviton\RqlParser\AbstractNode;
 use Graviton\RqlParser\NodeParser\SortNodeParser;
 
 class ProjectNodeParser extends SortNodeParser
 {
     function getNodeName(): string {
         return 'project';
+    }
+
+    function getNode(array $fields) : AbstractNode {
+        return new ProjectNode($fields);
     }
 
 }
